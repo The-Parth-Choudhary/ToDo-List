1716ad5399e328dd2e68cb8bebb62e9405af804d
Parth Choudhary <parth.dudhwal@gmail.com>
2023-07-13 16:39:55 +0530

    Changing the port and making it variable to work on web

--- app.js
+++ app.js
@@ -129,6 +129,11 @@ app.get("/about", function (req, res) {
     res.render("about");
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+let prot = process.env.PORT;
+if(port == null || port == ""){
+    port =3000;
+}
+
+app.listen(port, function () {
+    console.log("Server started successfully");
 });
